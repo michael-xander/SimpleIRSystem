2b6f02afa58829c46c6afa5d689a8cb8cc354e99
Michael <michaelkyeyune01@gmail.com>
2016-05-09 10:37:50 +0200

    Updated index to always generate files listing stemmed words breakdown for each ID

--- index.py
+++ index.py
@@ -84,19 +84,18 @@ for key in data:
 # document length/title file
 g.close ()
 
-# write file stem counts to file if blind relevance feedback is activated
-if parameters.use_blind_relevance_feedback:
-    try:
-        os.mkdir(collection+"_index_stem_count")
-    except:
-        pass
-    # write stem counts to file per key id
-    for stemmed_word in index:
-        for file_id in index[stemmed_word]:
-            # append the stem count to the respective ID file
-            f = open (collection+"_index_stem_count/"+file_id, "a")
-            print(stemmed_word, index[stemmed_word][file_id], sep=':', file=f)
-            f.close()
+# carry out generation of files listing stemmed words for each ID
+try:
+    os.mkdir(collection+"_index_stem_count")
+except:
+    pass
+# write stem counts to file per key id
+for stemmed_word in index:
+    for file_id in index[stemmed_word]:
+        # append the stem count to the respective ID file
+        f = open (collection+"_index_stem_count/"+file_id, "a")
+        print(stemmed_word, index[stemmed_word][file_id], sep=':', file=f)
+        f.close()
 
 # write inverted index to files
 try:
