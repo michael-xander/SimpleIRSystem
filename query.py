376c7c318f89dfea0f55d1eb410c301185804083
Michael <michaelkyeyune01@gmail.com>
2016-05-12 23:23:41 +0200

    Added base code for relevance feedback

--- query.py
+++ query.py
@@ -110,8 +110,63 @@ def main():
 
     # print top ten results
     result = sorted(accum, key=accum.__getitem__, reverse=True)
-    for i in range(min(len(result), 10)):
-        print("{0:10.8f} {1:5} {2}".format(accum[result[i]], result[i], titles[result[i]]))
+
+    if parameters.use_blind_relevance_feedback:
+        print("Utilising blind relevance feedback")
+
+        # obtain the IDs of the current top k documents
+        k = 10
+        num_relevant_docs = min(len(result), k)
+        relevant_document_ids = []
+        for i in range(num_relevant_docs):
+            relevant_document_ids.append(result[i])
+
+        # data structure to keep track of stemmed words in the relevant documents and the No. of documents they appear in
+        words_df_counter = {}
+        words_tf_counter = {}
+        for document_id in relevant_document_ids:
+            f = open(collection + "_index_stem_count/"+ document_id, "r")
+            lines = f.readlines()
+            for line in lines:
+                mo = re.match(r'([a-z0-9]+)\:([0-9\.]+)', line)
+                if mo:
+                    word = mo.group(1)
+                    count = eval(mo.group(2))
+
+                    if not word in words_tf_counter:
+                        words_tf_counter[word] = {}
+                    words_tf_counter[word][document_id] = count
+
+                    if not word in words_df_counter:
+                        words_df_counter[word] = 0
+                    words_df_counter[word] += 1
+
+        # data structure to contain stats on words
+        word_stats = {}
+        for word in words_df_counter:
+            df = words_df_counter[word]
+            idf = 1/df
+            if parameters.log_idf:
+                idf = math.log(1 + k/df)
+            for document_id in words_tf_counter[word]:
+                tf = float(words_tf_counter[word][document_id])
+                if parameters.log_tf:
+                    tf = (1 + math.log(tf))
+                if not word in word_stats:
+                    word_stats[word] = 0
+                word_stats[word] += (tf * idf)
+
+        # rank the words found in the documents
+        ranked_words = sorted(word_stats, key=word_stats.__getitem__, reverse=True)
+        indicative_number = 20
+        for i in range(min(len(ranked_words), indicative_number)):
+            if not ranked_words[i] in query_words:
+                query_words.append(ranked_words[i])
+
+        # do search again with new query words
+    else:
+        for i in range(min(len(result), 10)):
+            print("{0:10.8f} {1:5} {2}".format(accum[result[i]], result[i], titles[result[i]]))
 
 
 # run the main method
